75757557f78bd94a583ea2595b8ac161a7bff9b5
ethanryan <ethanryan@gmail.com>
2018-02-13 14:18:07 -0500

    adding state to LoginForm

--- login-form/src/components/LoginForm.js
+++ login-form/src/components/LoginForm.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
 
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import { Button, Form } from 'semantic-ui-react'
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state =
+    {
+      username: '',
+      password: ''
+    }
+    this.handleUsernameChange = this.handleUsernameChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+  }
+
+  handleUsernameChange(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePasswordChange(event) {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
   render() {
+    console.log('state from LoginForm: ', this.state)
     return (
       <div className="LoginForm">
         <Form>
           <Form.Field>
-            <label>First Name</label>
-            <input placeholder='First Name' />
+            <label>Username</label>
+            <input placeholder='Username' onChange={this.handleUsernameChange}/>
           </Form.Field>
           <Form.Field>
-            <label>Last Name</label>
-            <input placeholder='Last Name' />
-          </Form.Field>
-          <Form.Field>
-            <Checkbox label='I agree to the Terms and Conditions' />
+            <label>Password</label>
+            <input placeholder='Password' onChange={this.handlePasswordChange}/>
           </Form.Field>
           <Button type='submit'>Submit</Button>
         </Form>
