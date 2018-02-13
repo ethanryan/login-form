c7e3a3968787cf4af49151ce3bccc7cd625379d5
ethanryan <ethanryan@gmail.com>
2018-02-13 14:04:55 -0500

    created LoginForm component

---
+++ login-form/src/components/LoginForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <div className="LoginForm">
+        <p>
+          Login Form here...
+        </p>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
